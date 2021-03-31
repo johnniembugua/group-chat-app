55e18abb96d4d4e1b443ac34049e8492105f17cc
johnmbugua <johnniendungu321@gmail.com>
2021-03-31 11:18:20 +0300

    updated registration screen

--- lib/screens/registration_screen.dart
+++ lib/screens/registration_screen.dart
@@ -25,72 +25,69 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
         inAsyncCall: showSpinner,
         child: Padding(
           padding: EdgeInsets.symmetric(horizontal: 24.0),
-          child: SingleChildScrollView(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: <Widget>[
-                Flexible(
-                  child: Hero(
-                    tag: 'logo',
-                    child: Container(
-                      height: 150.0,
-                      child: Image.asset('images/logo.png'),
-                    ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Flexible(
+                child: Hero(
+                  tag: 'logo',
+                  child: Container(
+                    height: 120.0,
+                    child: Image.asset('images/logo.png'),
                   ),
                 ),
-                SizedBox(
-                  height: 48.0,
+              ),
+              SizedBox(
+                height: 48.0,
+              ),
+              TextField(
+                keyboardType: TextInputType.emailAddress,
+                textAlign: TextAlign.center,
+                onChanged: (value) {
+                  email = value;
+                },
+                decoration: KTextFieldDecoration.copyWith(
+                  hintText: 'Enter your email',
                 ),
-                TextField(
-                  keyboardType: TextInputType.emailAddress,
+              ),
+              SizedBox(
+                height: 8.0,
+              ),
+              TextField(
+                  obscureText: true,
                   textAlign: TextAlign.center,
                   onChanged: (value) {
-                    email = value;
+                    password = value;
                   },
                   decoration: KTextFieldDecoration.copyWith(
-                    hintText: 'Enter your email',
-                  ),
-                ),
-                SizedBox(
-                  height: 8.0,
-                ),
-                TextField(
-                    obscureText: true,
-                    textAlign: TextAlign.center,
-                    onChanged: (value) {
-                      password = value;
-                    },
-                    decoration: KTextFieldDecoration.copyWith(
-                      hintText: 'Enter your password',
-                    )),
-                SizedBox(
-                  height: 24.0,
-                ),
-                RoundedButton(
-                  color: Colors.blueAccent,
-                  onpressed: () async {
-                    setState(() {
-                      showSpinner = true;
-                    });
-                    try {
-                      final newUser =
-                          await _auth.createUserWithEmailAndPassword(
-                              email: email, password: password);
-                      if (newUser != null) {
-                        Navigator.pushNamed(context, ChatScreen.id);
-                      }
-                      setState(() {
-                        showSpinner = false;
-                      });
-                    } catch (e) {
-                      print(e);
+                    hintText: 'Enter your password',
+                  )),
+              SizedBox(
+                height: 24.0,
+              ),
+              RoundedButton(
+                color: Colors.blueAccent,
+                onpressed: () async {
+                  setState(() {
+                    showSpinner = true;
+                  });
+                  try {
+                    final newUser = await _auth.createUserWithEmailAndPassword(
+                        email: email, password: password);
+                    if (newUser != null) {
+                      Navigator.pushNamed(context, ChatScreen.id);
                     }
-                  },
-                  title: 'Register',
-                ),
-              ],
-            ),
+                    setState(() {
+                      showSpinner = false;
+                    });
+                  } catch (e) {
+                    print(e);
+                  }
+                },
+                title: 'Register',
+              ),
+            ],
           ),
         ),
       ),
